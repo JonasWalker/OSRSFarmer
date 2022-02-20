dd49c6aeba99d4f5ed14c29b4c4ef92d97583101
jonas <jonas@DESKTOP-C63S1T8>
2022-02-19 20:18:48 -0600

    Add project files.

---
+++ OSRSFarmer/MouseTracker.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace OSRSFarmer
+{
+    partial class MouseTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.oakTreeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.willowTreeButton = new System.Windows.Forms.Button();
+            this.YCoord = new System.Windows.Forms.Label();
+            this.XCoord = new System.Windows.Forms.Label();
+            this.YLabel = new System.Windows.Forms.Label();
+            this.XLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.oakTreeButton);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.timerLabel);
+            this.panel1.Controls.Add(this.willowTreeButton);
+            this.panel1.Controls.Add(this.YCoord);
+            this.panel1.Controls.Add(this.XCoord);
+            this.panel1.Controls.Add(this.YLabel);
+            this.panel1.Controls.Add(this.XLabel);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(315, 203);
+            this.panel1.TabIndex = 0;
+            // 
+            // oakTreeButton
+            // 
+            this.oakTreeButton.Location = new System.Drawing.Point(26, 167);
+            this.oakTreeButton.Name = "oakTreeButton";
+            this.oakTreeButton.Size = new System.Drawing.Size(75, 23);
+            this.oakTreeButton.TabIndex = 10;
+            this.oakTreeButton.Text = "Oak Tree";
+            this.oakTreeButton.UseVisualStyleBackColor = true;
+            this.oakTreeButton.Click += new System.EventHandler(this.oakTreeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.timerButton_Click);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(204, 27);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(71, 25);
+            this.timerLabel.TabIndex = 8;
+            this.timerLabel.Text = "00:00";
+            // 
+            // willowTreeButton
+            // 
+            this.willowTreeButton.Location = new System.Drawing.Point(26, 131);
+            this.willowTreeButton.Name = "willowTreeButton";
+            this.willowTreeButton.Size = new System.Drawing.Size(75, 23);
+            this.willowTreeButton.TabIndex = 7;
+            this.willowTreeButton.Text = "Willow Tree";
+            this.willowTreeButton.UseVisualStyleBackColor = true;
+            this.willowTreeButton.Click += new System.EventHandler(this.willowTreeButton_Click);
+            // 
+            // YCoord
+            // 
+            this.YCoord.AutoSize = true;
+            this.YCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YCoord.Location = new System.Drawing.Point(102, 74);
+            this.YCoord.Name = "YCoord";
+            this.YCoord.Size = new System.Drawing.Size(64, 25);
+            this.YCoord.TabIndex = 3;
+            this.YCoord.Text = "0000";
+            // 
+            // XCoord
+            // 
+            this.XCoord.AutoSize = true;
+            this.XCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XCoord.Location = new System.Drawing.Point(102, 27);
+            this.XCoord.Name = "XCoord";
+            this.XCoord.Size = new System.Drawing.Size(64, 25);
+            this.XCoord.TabIndex = 2;
+            this.XCoord.Text = "0000";
+            // 
+            // YLabel
+            // 
+            this.YLabel.AutoSize = true;
+            this.YLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YLabel.Location = new System.Drawing.Point(36, 74);
+            this.YLabel.Name = "YLabel";
+            this.YLabel.Size = new System.Drawing.Size(28, 25);
+            this.YLabel.TabIndex = 1;
+            this.YLabel.Text = "Y";
+            // 
+            // XLabel
+            // 
+            this.XLabel.AutoSize = true;
+            this.XLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XLabel.Location = new System.Drawing.Point(36, 27);
+            this.XLabel.Name = "XLabel";
+            this.XLabel.Size = new System.Drawing.Size(27, 25);
+            this.XLabel.TabIndex = 0;
+            this.XLabel.Text = "X";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MouseTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 202);
+            this.Controls.Add(this.panel1);
+            this.Name = "MouseTracker";
+            this.Text = "MouseTracker";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MouseTracker_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label YCoord;
+        private System.Windows.Forms.Label XCoord;
+        private System.Windows.Forms.Label YLabel;
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Button willowTreeButton;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button oakTreeButton;
+    }
+}
+
---
+++ OSRSFarmer/MouseTracker.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace OSRSFarmer
+{
+    public partial class MouseTracker : Form
+    {
+        public new System.Windows.Forms.Cursor Cursor { get; set; }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        private int _ticks = 0;
+
+        int startMousePositionX;
+        int startMousePositionY;
+
+        int bankBoothAtBankBoothX = 865;
+        int bankBoothAtBankBoothY = 530;
+
+        int willowTreeAtBankBoothX = 415;
+        int willowTreeAtBankBoothY = 850;
+
+        int willowTreeAtWillowTreeX = 925;
+        int willowTreeAtWillowTreeY = 629;
+
+        int bankBoothAtWillowTreeX = 1406;
+        int bankBoothAtWillowTreeY = 317;
+
+        int oakTreeAtOakTreeX = 1084;
+        int oakTreeAtOakTreeY = 603;
+
+        int bankBoothAtOakTreeX = 230;
+        int bankBoothAtOakTreeY = 652;
+
+        int oakTreeAtBankBoothX = 1646;
+        int oakTreeAtBankBoothY = 488;
+
+        int inventorySlot1X = 1697;
+        int inventorySlot1Y = 764;
+
+
+        //takes 12 second to walk from bankbooth to tree
+        //while standing at bankbooth. the bankbooth is at x865y530
+        //while standing at bankbooth. the tree is at x415y850
+        //while standing at tree. tree is at x925y629
+        //while standing at tree. bank booth is at x1406y317
+        //inventory slot 1 is at x1697y764
+
+        public MouseTracker()
+        {
+            InitializeComponent();
+
+            startMousePositionX = MousePosition.X;
+            startMousePositionY = MousePosition.Y;
+
+            XCoord.Text = startMousePositionX.ToString();
+            YCoord.Text = startMousePositionY.ToString();
+        }
+
+        private void MouseTracker_Load(object sender, EventArgs e)
+        {
+            //Tracker( sender, e);
+        }
+
+        //Left Clicks
+        public static void DoMouseLeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+
+        private void PlaceMouse(int x, int y)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(x,y);
+            XCoord.Text = Cursor.Position.X.ToString();
+            YCoord.Text = Cursor.Position.Y.ToString();
+        }
+
+        public void WaitSomeTime(int time)
+        {
+            System.Threading.Thread.Sleep(time);
+        }
+
+        
+
+        private void willowTreeButton_Click(object sender, EventArgs e)
+        {
+            //start timer
+            //timer1.Start();
+
+            //Place character in a certain position (infront of bank booth)
+            //Click start
+
+            //put cursor on the tree. With the saved coordinates of tree
+            PlaceMouse(willowTreeAtBankBoothX, willowTreeAtBankBoothY);
+            //DoMouseLeftClick on tree (walks over and starts cutting)
+            DoMouseLeftClick();
+            //Wait 35 seconds
+            WaitSomeTime(35000);
+
+            //////while loop begins(waits for escape button)
+            while (true)
+            {
+                //move cursor over tree
+                PlaceMouse(willowTreeAtWillowTreeX, willowTreeAtWillowTreeY);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                ///////////inventory should be full/////////
+                //place cursor on bank booth
+                PlaceMouse(bankBoothAtWillowTreeX, bankBoothAtWillowTreeY);
+                //DoMouseLeftClick on bank booth(makes you walk towards booth and opens your bank)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //move cursor to inventory on slot 1
+                PlaceMouse(inventorySlot1X, inventorySlot1Y);
+                //DoMouseLeftClick on slot 1(to empty inventory)
+                DoMouseLeftClick();
+                //place cursor on tree
+                PlaceMouse(willowTreeAtBankBoothX, willowTreeAtBankBoothY);
+                //DoMouseLeftClick on tree (makes you walk to tree)
+                DoMouseLeftClick();
+                //Wait 35 seconds
+                WaitSomeTime(35000);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _ticks++;
+            timerLabel.Text = _ticks.ToString();
+        }
+
+        private void timerButton_Click(object sender, EventArgs e)
+        {
+            //timer1.Start();
+            /*if(timer1.Enabled == true)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }*/
+            WaitSomeTime(3000);
+            XCoord.Text = Cursor.Position.X.ToString();
+            YCoord.Text = Cursor.Position.Y.ToString();
+        }
+
+        private void oakTreeButton_Click(object sender, EventArgs e)
+        {
+            //start timer
+            timer1.Start();
+
+            //Place character in a certain position (infront of bank booth)
+            //Click start
+
+            //put cursor on the tree. With the saved coordinates of tree
+            PlaceMouse(oakTreeAtBankBoothX, oakTreeAtBankBoothY);
+            //DoMouseLeftClick on tree (walks over and starts cutting)
+            DoMouseLeftClick();
+            //Wait 35 seconds
+            WaitSomeTime(35000);
+
+            //////while loop begins(waits for escape button)
+            while (true)
+            {
+                //move cursor over tree
+                PlaceMouse(oakTreeAtOakTreeX, oakTreeAtOakTreeY);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //DoMouseLeftClick on tree again(assuming the tree was cut down and spawned again)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                ///////////inventory should be full/////////
+                //place cursor on bank booth
+                PlaceMouse(bankBoothAtOakTreeX, bankBoothAtOakTreeY);
+                //DoMouseLeftClick on bank booth(makes you walk towards booth and opens your bank)
+                DoMouseLeftClick();
+                //Wait 20 seconds
+                WaitSomeTime(20000);
+                //move cursor to inventory on slot 1
+                PlaceMouse(inventorySlot1X, inventorySlot1Y);
+                //DoMouseLeftClick on slot 1(to empty inventory)
+                DoMouseLeftClick();
+                //place cursor on tree
+                PlaceMouse(oakTreeAtBankBoothX, oakTreeAtBankBoothY);
+                //DoMouseLeftClick on tree (makes you walk to tree)
+                DoMouseLeftClick();
+                //Wait 35 seconds
+                WaitSomeTime(35000);
+            }
+        }
+    }
+}
